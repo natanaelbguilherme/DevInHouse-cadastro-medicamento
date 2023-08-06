435c578d048217807bcf17d3b8ed9ef3276c9d33
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-08-06 09:29:01 -0300

    criação do componente do formulario

--- cadmed/src/App.vue
+++ cadmed/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <Header />
+    <FormularioNovoMedicamento @cadastrar="cadastrarMedicamento" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento.vue";
 
 export default {
   components: {
     Header,
+    FormularioNovoMedicamento,
+  },
+  methods: {
+    cadastrarMedicamento() {
+      console.log("clicou");
+    },
   },
 };
 </script>
---
+++ cadmed/src/components/FormularioNovoMedicamento.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" v-model="medicamento" />
+
+    <label for="laboratorio">Laboratório</label>
+    <input type="text" id="laboratorio" v-model="laboratorio" />
+
+    <label for="preco">Preço</label>
+    <input type="number" id="preco" v-model="preco" />
+
+    <button @click="$emit('cadastrar')">Cadastrar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      medicamento: "",
+      laboratorio: "",
+      preco: 1,
+    };
+  },
+};
+</script>
+
+<style></style>
